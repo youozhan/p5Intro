854a91124aa7fef53c5e1b778c8682286d546064
Youchun <30670835+youozhan@users.noreply.github.com>
2019-10-15 21:33:41 -0400

    Add user input

--- 02/js/sketch.js
+++ 02/js/sketch.js
@@ -1,67 +1,72 @@
-
 // A wind direction vector
-let wind;
-// Circle position
-let position;
 
-function setup() {
-  createCanvas(720, 200);
-  // Request the data from apixu
-  let url = 'http://api.weatherstack.com/current?access_key=bea4af6c1510c5f3935b58dfd760c015&query=London';
+var wind;
+var position;
+var input;
+
+function setup(){
+  createCanvas(720,200);
+
+  var url = 'http://api.weatherstack.com/current?access_key=bea4af6c1510c5f3935b58dfd760c015&query=London';
   loadJSON(url, gotWeather);
-  // Circle starts in the middle
+
+  var button = select('#submit');
+  button.mousePressed(askWeather);
+  input = select('#city');
+
   position = createVector(width/2, height/2);
-  // wind starts as (0,0)
   wind = createVector();
 }
 
-function draw() {
+function draw(){
   background(200);
 
-  // This section draws an arrow pointing in the direction of wind
+  // Arrow pointing out the wind direction
   push();
-  translate(32, height - 32);
-  // Rotate by the wind's angle
-  rotate(wind.heading() + PI/2);
+  translate(32,height-32);
+  //insert a variable
+  rotate(wind.heading()+PI/2);
   noStroke();
   fill(255);
-  ellipse(0, 0, 48, 48);
+  ellipse(0,0,48,48);
 
-  stroke(45, 123, 182);
+  stroke(45,123,182);
   strokeWeight(3);
-  line(0, -16, 0, 16);
+  line(0,-16,0,16);
 
   noStroke();
-  fill(45, 123, 182);
-  triangle(0, -18, -6, -10, 6, -10);
+  fill(45,123,182);
+  triangle(0,-18,-6,-10,6,-10);
+
   pop();
-  
-  // Move in the wind's direction
+
+  // Create a shape following the wind
   position.add(wind);
-  
+
   stroke(0);
   fill(51);
   ellipse(position.x, position.y, 16, 16);
 
-  if (position.x > width)  position.x = 0;
-  if (position.x < 0)      position.x = width;
-  if (position.y > height) position.y = 0;
-  if (position.y < 0)      position.y = height;
+  if(position.x > width) position.x = 0;
+  if(position.x < 0) position.x = width;
+  if(position.y > height) position.y = 0;
+  if(position.y < 0) position.y = height;
 }
 
-function gotWeather(weather) {
+function askWeather(){
+  // Get data from weatherstack.com
+  //var url = 'http://api.weatherstack.com/current?access_key=bea4af6c1510c5f3935b58dfd760c015&query=London';
   
-  // Get the angle (convert to radians)
-  let angle = radians(Number(weather.current.wind_degree));
-  console.log(angle);
-  // Get the wind speed
-  let windmag = Number(weather.current.wind_speed);
-  console.log(windmag);
-  
-  // Display as HTML elements
-  let temperatureDiv = createDiv(floor(weather.current.temperature) + '&deg;');
-  let windDiv = createDiv("WIND " + windmag + " <small>MPH</small>");
-  
-  // Make a vector
+  var api = 'http://api.weatherstack.com/current?access_key=bea4af6c1510c5f3935b58dfd760c015&query=';
+  // var city = 'London';
+  var url = api + input.value();
+
+  loadJSON(url,gotWeather);
+}
+
+function gotWeather(weather){
+  var angle = radians(Number(weather.current.wind_degree));
+  var windmag = Number(weather.current.wind_speed);
+
   wind = p5.Vector.fromAngle(angle);
 }
